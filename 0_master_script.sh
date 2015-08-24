4f0f77ca64e8d6f2947b90b5a04c6849c52c5804
portdevil <portdevil@gmail.com>
2015-08-24 15:46:49 -0700

    Rename PANO images, set timestamps from filesnames, and optimize compression stage by running in bg

--- 0_master_script.sh
+++ 0_master_script.sh
@@ -1,6 +1,6 @@
 cd "/Users/jonathan/Dropbox/DropSync Uploads"
 
-# Confirmation dialogue to avoid disasters.
+# Confirm directory to avoid disasters
 echo -e "\nCurrent directory is $PWD.\n\nWARNING: This program does not make any backups of the source material. Using this script in an improperly structured folder can lead to catastrophic results.\n"
 read -p "Are you sure you want to continue (y/n)? " choice
 case "$choice" in 
@@ -14,7 +14,7 @@ case "$choice" in
   ;;
 esac
 
-# Move everything to root folder and delete the sub folders.
+# Move everything to root folder and delete the sub folders
 for dir in */ ; do
   cd "$dir"
   for file in * ; do
@@ -24,17 +24,17 @@ for dir in */ ; do
   rmdir "$dir" > /dev/null 2>&1
 done
 
-# Rename JPG to jpg.
+# Rename JPG to jpg
 for i in $(ls *.JPG 2> /dev/null) ; do
   mv -f $i ${i%.JPG}.jpg
 done
 
-# Rename DNG to dng.
+# Rename DNG to dng
 for i in $(ls *.DNG 2> /dev/null) ; do
   mv -f $i ${i%.DNG}.dng
 done
 
-# Rename IMG_ format images from Google Camera.
+# Rename IMG_ format images from Google Camera
 for i in $(ls *.jpg 2> /dev/null) ; do
   first=`echo ${i:0:4}`
   if [ $first = 'IMG_' ]
@@ -44,14 +44,37 @@ for i in $(ls *.jpg 2> /dev/null) ; do
   fi
 done
 
-# Sorts files into 2015-XX-XX folders.
+# Rename PANO_ format images from Google Camera
+for i in $(ls *.jpg 2> /dev/null) ; do
+  first=`echo ${i:0:5}`
+  if [ $first = 'PANO_' ]
+  then
+    new=`echo ${i:5:4}-${i:9:2}-${i:11:2}-${i:14:2}-${i:16:2}-${i:18:2}.jpg`
+    mv "$i" "$new"
+  fi
+done
+
+# Set timestamps from file name
+for i in $(ls *.jpg 2> /dev/null) ; do
+  year=`echo ${i:0:4}`
+  month=`echo ${i:5:2}`
+  date=`echo ${i:8:2}`
+  hour=`echo ${i:11:2}`
+  minute=`echo ${i:14:2}`
+  second=`echo ${i:17:2}`
+  full_date=`echo $month/$date/$year $hour:$minute:$second`
+  SetFile -d "$full_date" $i
+  SetFile -m "$full_date" $i
+done
+
+# Sort files into 2015-XX-XX folders
 for i in $(ls *.jpg *.dng *.mp4 2> /dev/null) ; do
   dir=`echo ${i:0:8}XX Daily Life`
   mkdir "$dir" > /dev/null 2>&1
   mv "$i" "$dir"
 done
 
-# Move files to proper location.
+# Move files to proper location
 for dir in */ ; do
   cd "$dir"
   dest="/Users/jonathan/Pictures/Memories/2015/$dir"
@@ -64,10 +87,10 @@ for dir in */ ; do
   rmdir "$dir"
 done
 
-# Stage 1 is finished.
+# Finish Stage 1
 echo "Finished with moving files to Pictures folder!"
 
-# Confirmation dialogue to proceed to compression stage.
+# Confirm to proceed to compression stage
 echo -e "\nNow is the time to organize your files in the Pictures folder.\n"
 read -p "Are you ready to continue to the compression stage (y/n)? " choice
 case "$choice" in 
@@ -81,19 +104,19 @@ case "$choice" in
   ;;
 esac
 
-# Move to the Memories folder to simplify paths.
+# Move to the Memories folder to simplify paths
 cd ~/Pictures/Memories/
 
-# Sets output directory
+# Set output directory
 output_f="/Users/jonathan/Dropbox/Compressed Memories"
 
-# Recreates directory structure under 2015
+# Recreate directory structure under 2015
 for dir in 2015/*/
 do
   mkdir "$output_f/$dir" > /dev/null 2>&1
 done
 
-# Recreates sub-directory structure in 2015 folders
+# Recreate sub-directory structure in 2015 folders
 for dir in 2015/**/*/
 do
   if [[ "$dir" == *"Facebook"* ]] # Skip if Facebook in name.
@@ -103,7 +126,9 @@ do
   mkdir "$output_f/$dir" > /dev/null 2>&1
 done
 
-# Compresses files that haven't been compressed yet
+# Compress files that haven't been compressed yet
+start_time=$(date +%s)
+
 for f in 2015/* 2015/**/* 2015/**/**/*
 do
   ext=${f: -4}
@@ -112,11 +137,10 @@ do
     continue # Skip if the extension is not a JPG.
   fi
 
-  check_path="$output_f/$f"
   output_path="$output_f/$f"
-  if [ ! -f "$check_path" ] # Skip if image is already compressed.
+  if [ ! -f "$output_path" ] # Skip if image is already compressed.
   then
-    if [ -d "$check_path" ] # Skip if working with a folder.
+    if [ -d "$output_path" ] # Skip if working with a folder.
     then
       continue
     fi
@@ -124,9 +148,14 @@ do
     then
       continue
     fi
-    convert "$f" -resize "1080^>" -quality 60% "$output_path" > /dev/null 2>&1
+    convert "$f" -resize "1080^>" -quality 60% "$output_path" & > /dev/null 2>&1
     echo "Created new file $output_path"
   fi
 done
+
+wait
+end_time=$(date +%s)
+
+echo "Compression operations took $(($end_time - $start_time)) seconds."
 
 echo "Finished!"
